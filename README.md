ebb50980b4c3222ebdda0c8bd185da6aa161d666
flber <benrhammond@gmail.com>
2020-12-17 04:57:33 -0800

    updated readme

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Pillar
 A simple site generator with template support, written in plain Rust with no dependencies.
 
-Soon to be used in [[my site]](https://mineralexistence.com)
+Used in [[my site]](https://mineralexistence.com)
 
 A nice little demo is available in the `examples/` folder of this repo, which shows off Pillar's features (and honestly is mostly a place for me to test and debug)
 
@@ -12,11 +12,13 @@ Pillar uses the marble markup format, which is quite similar to markdown but wit
 - Unordered lists still use `-`, but it's far less picky about initial indentation than some markdown parsers (I'm looking at you cmark) and supports weird changes in indentation
 - Ordered lists just use `~`, so no need for manually numbering your lists. Same indentation support as ordered lists.
 - Blockquotes use the same syntax as markdown, a `>` with as much or as little whitespace before and after as you want.
+- {{date}} replaces with the date the content was last modified (the marble, not the html itself)
 
 I'm also planning on adding several more niche elements to the marble format, so that list above will grow over time.
 
 Pillar also does some html substitutions to make templates more interesting
 - {{content}} is replaced with the content of a give marble page, parsed into html (sort of necessary)
+- {{date}} has the same behaviour as in marble
 
 Lot's of updates and improvements to come!
 
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::fs;
+use std::os::unix::fs::MetadataExt;
 
 const HELP_MENU: &str = "Builds static site from marble files \n\
 						 \n\
@@ -22,133 +23,150 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
         println!("{}", HELP_MENU);
-        return ()
+        return ();
     }
 
     let instruction = args[1].as_str();
 
     match instruction {
-    	"-V" => println!("Pillar version {}", env!("CARGO_PKG_VERSION")),
-    	"build" => {
-    		// gets the marble files
-    		match fs::read_dir(MARBLE_PATH) {
-    			// unpacks the entries result
-    			Ok(entries) => {
-    				// goes through the entry objects
-    				println!("Parsing pages...");
-    				for entry in entries {
-    					// unpacks the entry result
-    					match entry {
-    						Ok(entry) => {
-    							// parses the file
-    							let path = format!("{:?}", entry.path());
-    							let path_str = &path[1..path.len()-1];
-    							
-    							let contents = fs::read_to_string(path_str)
-    								.expect(format!("Something went wrong reading {}", path_str).as_str());
+        "-V" => println!("Pillar version {}", env!("CARGO_PKG_VERSION")),
+        "build" => {
+            // gets the marble files
+            match fs::read_dir(MARBLE_PATH) {
+                // unpacks the entries result
+                Ok(entries) => {
+                    // goes through the entry objects
+                    println!("Parsing pages...");
+                    for entry in entries {
+                        // unpacks the entry result
+                        match entry {
+                            Ok(entry) => {
+                                // parses the file
+                                let path = format!("{:?}", entry.path());
+                                let path_str = &path[1..path.len() - 1];
 
-    							let split_contents = contents.lines();
-   							    let str_lines: Vec<&str> = split_contents.collect();
-   							    let mut lines = Vec::<String>::new();
-   							
-   							    for str_line in str_lines {
-   							        let mut line = String::new();
-   							        line.push_str(str_line);
-   							        lines.push(line);
-   							    }
-   							
-   							    let parsed = parse_marble(lines).join("");
+                                let contents = fs::read_to_string(path_str).expect(
+                                    format!("Something went wrong reading {}", path_str).as_str(),
+                                );
 
-								let default_template_path = [TEMPLATE_PATH, "default.html"].concat();
-   							    let template_contents = fs::read_to_string(default_template_path)
-   							    	.expect("couldn't load default template");
+                                let metadata = fs::metadata(path_str).expect(
+                                    format!("couldn't read metadata from {}", path_str).as_str(),
+                                );
+                                let last_modified = &metadata.mtime().to_string();
+                                let date = calc_date(last_modified.to_string());
+                                let short_date = format!("{}{}{}", date.0, date.1, &date.2[2..]);
+                                
+                                let split_contents = contents.lines();
+                                let str_lines: Vec<&str> = split_contents.collect();
+                                let mut lines = Vec::<String>::new();
 
-   							    let page = replace(&template_contents, "{{content}}", &parsed);
+                                for str_line in str_lines {
+                                    let mut line = String::new();
+                                    line.push_str(str_line);
+                                    lines.push(line);
+                                }
 
-								let target = [HTML_PATH, &path[MARBLE_PATH.len()+1..&path.len()-3], "html"].concat();
-								println!("+ {}", target);
-   							    match fs::write(&target, &page) {
-							    	Ok(_) => (),
-							    	Err(e) => println!("failed to write to {}: {}", &target, e),
-							    };
-   							
-    						},
-    						Err(e) => {
-    							println!("Failed to open entry with error {}", e);
-    						}
-    					}
-    				}
-    			},
-    			Err(e) => {
-    				println!("Failed to open directory {} with error {}", MARBLE_PATH, e);
-    				return ()
-    			}
-    		}
-    	},
-    	"clean" => {
-		 	match fs::read_dir(HTML_PATH) {
-    			// unpacks the entries result
-    			Ok(entries) => {
-    				// goes through the entry objects
-    				println!("Deleting html...");
-    				for entry in entries {
-    					// unpacks the entry result
-    					match entry {
-    						Ok(entry) => {
-    							// parses the file
-    							let path = format!("{:?}", entry.path());
-    							let path_str = &path[1..path.len()-1];
-    							println!("- {}", path_str);
-    							if &path_str[path_str.len()-4..] == "html" {
-	    							match fs::remove_file(path_str) {
-	    								Ok(_) => (),
-	    								Err(e) => println!("failed to delete file {} with error {}", path_str, e)
-	    							}
-    							}
-    						}
-    						Err(e) => {
-						    	println!("Failed to open entry with error {}", e);
-						    }
-						}
-					}
-				},
-   				Err(e) => {
-    				println!("Failed to open directory {} with error {}", HTML_PATH, e);
-    				return ()
-    			}
-    		}
-    	},
-    	_ => println!("{}", HELP_MENU)
+                                let parsed = parse_marble(lines).join("");
+
+                                let default_template_path =
+                                    [TEMPLATE_PATH, "default.html"].concat();
+                                let template_contents = fs::read_to_string(default_template_path)
+                                    .expect("couldn't load default template");
+
+                                let page = replace(&template_contents, "{{content}}", &parsed);
+                                let page = replace(&page, "{{date}}", &short_date);
+
+                                let target = [
+                                    HTML_PATH,
+                                    &path[MARBLE_PATH.len() + 1..&path.len() - 3],
+                                    "html",
+                                ]
+                                .concat();
+                                println!("+ {}", target);
+                                match fs::write(&target, &page) {
+                                    Ok(_) => (),
+                                    Err(e) => println!("failed to write to {}: {}", &target, e),
+                                };
+                            }
+                            Err(e) => {
+                                println!("Failed to open entry with error {}", e);
+                            }
+                        }
+                    }
+                }
+                Err(e) => {
+                    println!("Failed to open directory {} with error {}", MARBLE_PATH, e);
+                    return ();
+                }
+            }
+        }
+        "clean" => {
+            match fs::read_dir(HTML_PATH) {
+                // unpacks the entries result
+                Ok(entries) => {
+                    // goes through the entry objects
+                    println!("Deleting html...");
+                    for entry in entries {
+                        // unpacks the entry result
+                        match entry {
+                            Ok(entry) => {
+                                // parses the file
+                                let path = format!("{:?}", entry.path());
+                                let path_str = &path[1..path.len() - 1];
+                                println!("- {}", path_str);
+                                if &path_str[path_str.len() - 4..] == "html" {
+                                    match fs::remove_file(path_str) {
+                                        Ok(_) => (),
+                                        Err(e) => println!(
+                                            "failed to delete file {} with error {}",
+                                            path_str, e
+                                        ),
+                                    }
+                                }
+                            }
+                            Err(e) => {
+                                println!("Failed to open entry with error {}", e);
+                            }
+                        }
+                    }
+                }
+                Err(e) => {
+                    println!("Failed to open directory {} with error {}", HTML_PATH, e);
+                    return ();
+                }
+            }
+        }
+        _ => println!("{}", HELP_MENU),
     }
 }
 
 fn parse_marble(lines: Vec<String>) -> Vec<String> {
     let mut output = Vec::<String>::new();
 
-	// sets lines which shouldn't be parsed (code and page variables)
+    // sets lines which shouldn't be parsed (code and page variables)
     let mut reserved = Vec::<String>::new();
-	let mut in_reserved = false;
+    let mut in_reserved = false;
     for i in 0..lines.len() {
-    	let mut line = lines[i].clone();
-    	if line.len() > 5 {
-	    	let first = first(&line).1;
-	    	if &line[first..first+6] == "!code!" && !in_reserved {
-	    		in_reserved = true;
-	    		line = String::from("<pre><code>");
-	    	} else if &line[first..first+6] == "!code!" && in_reserved {
-	    		line = String::from("</code></pre>");
-	    		in_reserved = false;
-	    	} else if in_reserved {
-	    		line.push_str("\n");
-	    		reserved.push(line);
-	    		line = String::from("!reserved!");
-	    	}
-    	}
-    	output.push(line);
+        let mut line = lines[i].clone();
+        let first = first(&line).1;
+        if line.len() >= first + 6 {
+            if &line[first..first + 6] == "!code!" && !in_reserved {
+                in_reserved = true;
+                line = String::from("<pre><code>");
+            } else if &line[first..first + 6] == "!code!" && in_reserved {
+                line = String::from("</code></pre>");
+                in_reserved = false;
+            } else if in_reserved {
+                line.push_str("\n");
+                reserved.push(line);
+                line = String::from("!reserved!");
+            }
+        }
+        output.push(line);
     }
 
     // single line formatting goes in here
-   for i in 0..output.len() {
+    for i in 0..output.len() {
         if output[i].len() > 0 {
             output[i] = h(&output[i]);
             output[i] = em(&output[i]);
@@ -163,45 +181,45 @@ fn parse_marble(lines: Vec<String>) -> Vec<String> {
     output = ol(&output);
     output = blockquote(&output);
 
-	output = p(&output);
+    output = p(&output);
     output = nl(&output);
 
-	let mut reserved_index = 0;
+    let mut reserved_index = 0;
     for i in 0..output.len() {
-    	if output[i].contains("!reserved!") {
-    		output[i] = reserved[reserved_index].clone();
-    		reserved_index += 1;
-    	}
+        if output[i].contains("!reserved!") {
+            output[i] = reserved[reserved_index].clone();
+            reserved_index += 1;
+        }
     }
 
     return output;
 }
 
 fn nl(l: &Vec<String>) -> Vec<String> {
-	let mut output = Vec::<String>::new();
-	for i in 0..l.len() {
-		let mut line = l[i].clone();
-		line.push_str("\n");
-		output.push(line.to_string());
-	}
-	return output
+    let mut output = Vec::<String>::new();
+    for i in 0..l.len() {
+        let mut line = l[i].clone();
+        line.push_str("\n");
+        output.push(line.to_string());
+    }
+    return output;
 }
 
 fn p(l: &Vec<String>) -> Vec<String> {
-	let mut output = Vec::<String>::new();
-	for i in 0..l.len() {
-		let mut line = l[i].clone();
-		if line.len() > 4 {
-			let i_first = first(&line).1;
-			let four = &line[i_first..i_first+4];
-			if four == "<em>" || four == "<a h" || four == "<img" || first(&line).0 != "<" {
-				line.insert_str(0, "<p>");
-				line.push_str("</p>");
-			}
-		}
-		output.push(line.to_string());
-	}
-	return output
+    let mut output = Vec::<String>::new();
+    for i in 0..l.len() {
+        let mut line = l[i].clone();
+        let i_first = first(&line).1;
+        if line.len() >= i_first + 4 {
+            let four = &line[i_first..i_first + 4];
+            if four == "<em>" || four == "<a h" || four == "<img" || first(&line).0 != "<" {
+                line.insert_str(0, "<p>");
+                line.push_str("</p>");
+            }
+        }
+        output.push(line.to_string());
+    }
+    return output;
 }
 
 fn blockquote(l: &Vec<String>) -> Vec<String> {
@@ -211,17 +229,17 @@ fn blockquote(l: &Vec<String>) -> Vec<String> {
         let mut line = l[i].clone();
         let char = first(&line).0;
 
-		let mut is_blockquote = false;
-		if char == ">" {
-			output.push(String::from("<blockquote>"));
-			line = remove(&line, first(&line).1, 1);
-			line = remove(&line, 0, first(&line).1);
-			is_blockquote = true;
-		}
-        
+        let mut is_blockquote = false;
+        if char == ">" {
+            output.push(String::from("<blockquote>"));
+            line = remove(&line, first(&line).1, 1);
+            line = remove(&line, 0, first(&line).1);
+            is_blockquote = true;
+        }
+
         output.push(line.to_string());
         if is_blockquote {
-			output.push(String::from("</blockquote>"));	
+            output.push(String::from("</blockquote>"));
         }
         i += 1;
     }
@@ -356,7 +374,7 @@ fn img(s: &String) -> String {
     while i < line.len() {
         let char = &line[i..i + 1];
         let prev_char = &line[i - 1..i];
-        if &line[i..i+1] == "[" && prev_char == "!" && !in_bracket {
+        if &line[i..i + 1] == "[" && prev_char == "!" && !in_bracket {
             in_bracket = true;
             i_bracket = i;
         }
@@ -375,15 +393,15 @@ fn img(s: &String) -> String {
             // removes the closing bracket and link portion
             line = remove(&line, i_paren - 1, i - i_paren + 2);
             // removes opening bracket and !
-            line = remove(&line, i_bracket-1, 2);
+            line = remove(&line, i_bracket - 1, 2);
             // inserts img and alt
-            line.insert_str(i_bracket-1, "<img alt=\"");
+            line.insert_str(i_bracket - 1, "<img alt=\"");
             // adds src to img
-            line.insert_str(i_bracket+4, "src=\"");
+            line.insert_str(i_bracket + 4, "src=\"");
             // adds image link to src
-            line.insert_str(i_bracket+9, &link);
+            line.insert_str(i_bracket + 9, &link);
             // closes src
-            line.insert_str(i_bracket+9 + &link.len(), "\" ");
+            line.insert_str(i_bracket + 9 + &link.len(), "\" ");
         }
         i += 1;
     }
@@ -459,17 +477,17 @@ fn h(s: &String) -> String {
     let mut line = String::new();
     let mut is_header = false;
 
-    if &s[..3] == "###" {
+    if s.len() > 2 && &s[..3] == "###" {
         line = remove(&s, 0, 3);
         line.insert_str(0, "<h3>");
         line.insert_str(line.len(), "</h3>");
         is_header = true;
-    } else if &s[..2] == "##" {
+    } else if s.len() > 1 && &s[..2] == "##" {
         line = remove(&s, 0, 2);
         line.insert_str(0, "<h2>");
         line.insert_str(line.len(), "</h2>");
         is_header = true;
-    } else if &s[..1] == "#" {
+    } else if s.len() > 0 && &s[..1] == "#" {
         line = remove(&s, 0, 1);
         line.insert_str(0, "<h1>");
         line.insert_str(line.len(), "</h1>");
@@ -527,10 +545,14 @@ fn insert(s: &String, idx: usize, ins: &str) -> String {
 */
 
 fn replace(s: &String, target: &str, insert: &str) -> String {
-	let mut source = s.clone();
-	let i = source.find(target).unwrap();
-	source.replace_range(i..i+target.len(), insert);
-	return source.to_string()
+    let mut source = s.clone();
+	loop {
+		match source.find(target) {
+			Some(i) => source.replace_range(i..i + target.len(), insert),
+			None => break,
+		}
+	}
+    return source.to_string();
 }
 
 fn remove(s: &String, idx: usize, len: usize) -> String {
@@ -556,3 +578,57 @@ fn first(s: &String) -> (String, usize) {
     return (String::from(""), num);
 }
 
+fn calc_date(s: String) -> (String, String, String) {
+	let mut _seconds = s.clone().parse::<i32>().unwrap();
+    let mut _minutes = _seconds / 60;
+    _seconds -= _minutes * 60;
+
+    let mut _hours = _minutes / 60;
+    _minutes -= _hours * 60;
+
+    let mut days = _hours / 24;
+    _hours -= days * 24;
+
+    /* Unix time starts in 1970 on a Thursday */
+    let mut year = 1970;
+    let mut month = 0;
+    let mut _day_of_week = 4;
+
+    loop {
+        let leap_year = year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
+        let days_in_year = if leap_year { 366 } else { 365 };
+        if days >= days_in_year {
+            _day_of_week += if leap_year { 2 } else { 1 };
+            days -= days_in_year;
+            if _day_of_week >= 7 {
+                _day_of_week -= 7;
+            }
+            year += 1;
+        } else {
+            _day_of_week += days;
+            _day_of_week %= 7;
+
+            /* calculate the month and day */
+            let days_in_month = vec![31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+            for m in 0..12 {
+                let mut dim = days_in_month[m];
+
+                /* add a day to February if this is a leap year */
+                if m == 1 && leap_year {
+                    dim += 1;
+                }
+
+                if days >= dim {
+                    days -= dim;
+                } else {
+                	month = m;
+                    break;
+                }
+            }
+            break;
+        }
+    }
+    days += 1;
+    month += 1;
+    return (days.to_string(), month.to_string(), year.to_string());
+}
